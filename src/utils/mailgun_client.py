1aef0e2333afcdd14cb396a91791d6a94677292b
Christopher Marik <hoefler@amfluss.info>
2025-07-19 07:21:16 +0200

    Add dummy mailgun_client.py for deployment

---
+++ src/utils/mailgun_client.py
@@ -0,0 +1,3 @@
+def send_email(to: str, subject: str, body: str):
+    # Dummy-Implementierung für Deployment. Hier echte Mailgun-Integration ergänzen!
+    print(f"[MAILGUN-FAKE] Sending email to {to}: {subject}\n{body}")
