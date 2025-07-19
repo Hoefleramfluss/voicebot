4a238b0a59a37a901962831ea25bd552397a8191
Christopher Marik <hoefler@amfluss.info>
2025-07-19 14:03:54 +0200

    Switch to Google Speech-to-Text v1 client for streaming recognition

--- src/speech_to_text/client.py
+++ src/speech_to_text/client.py
@@ -1,4 +1,4 @@
-from google.cloud import speech_v2 as speech
+from google.cloud import speech_v1 as speech
 from config.config import Config
 import os
 import json
@@ -7,30 +7,15 @@ class SpeechToTextClient:
     def __init__(self):
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = Config.GOOGLE_APPLICATION_CREDENTIALS
         self.client = speech.SpeechClient()
-        # Lies PROJECT_ID aus Credentials
-        with open(Config.GOOGLE_APPLICATION_CREDENTIALS, "r") as f:
-            creds = json.load(f)
-        self.project_id = "bubbly-hexagon-465711-p2"
-        self.location = "global"
-        self.recognizer_id = "wwd2"
 
-    def streaming_recognize(self, audio_generator, language_code="de-AT", sample_rate=8000):
+    def streaming_recognize(self, audio_generator, language_code="de-DE", sample_rate=8000):
         config = speech.RecognitionConfig(
-            auto_decoding_config=speech.AutoDetectDecodingConfig(),
-            language_codes=[language_code],
-            model="latest_long",
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=sample_rate,
+            language_code=language_code,
         )
-        streaming_config = speech.StreamingRecognitionConfig(
-            config=config,
-        )
-        recognizer = f"projects/{self.project_id}/locations/{self.location}/recognizers/{self.recognizer_id}"
         def request_generator():
-            # Erstes Paket: Konfiguration + Recognizer
-            yield speech.StreamingRecognizeRequest(
-                recognizer=recognizer,
-                streaming_config=streaming_config
-            )
-            # Danach: Audiodaten
+            yield speech.StreamingRecognizeRequest(streaming_config=speech.StreamingRecognitionConfig(config=config))
             for chunk in audio_generator:
                 yield speech.StreamingRecognizeRequest(audio_content=chunk)
         return self.client.streaming_recognize(requests=request_generator())
